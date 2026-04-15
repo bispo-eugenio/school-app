42a29a7a1e91c78f8de31569c1aa19fe01ff0d7c
bispo-eugenio <bispoeugenio270@gmail.com>
2026-04-15 08:54:54 -0300

    feat: adiciona conexão com EF Core + MySql e Adiciona o Swagger no projeto

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace schoolApi.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public static readonly string _connectionString = $"""
+        Server={Environment.GetEnvironmentVariable("SERVER")};
+        User ID={Environment.GetEnvironmentVariable("USERID")};
+        Password={Environment.GetEnvironmentVariable("PASSWORD")};
+        Database={Environment.GetEnvironmentVariable("DATABASE")}
+    """;
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using schoolApi.Data;
+using dotenv.net;
+
+DotEnv.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>((options) =>
+{
+    options.UseMySQL(ApplicationDbContext._connectionString);
+});
 
 var app = builder.Build();
 
@@ -10,6 +20,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
